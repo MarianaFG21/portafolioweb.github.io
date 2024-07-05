660563e8cb3602c2d68b81bf6752ed34593b3d2e
MarianaFG21 <marianafgodoy21@gmail.com>
2024-07-05 11:46:02 -0500

    segundo commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"bb10c5466638e963479ba5e64e601e42d1a434
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4046595607"
+    serviceWorkerVersion: "2739644520"
   }
 });
